3007fdeed5ff3f3d1a638ac2b18a87b271df9789
watanave <susan.se3p@gmail.com>
2020-01-20 22:31:06 +0900

    Initial commit
    Xcodeプロジェクト、初期テンプレートの状態。

---
+++ HiraganaTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiraganaTranslator
+//
+//  Created by Yohta Watanave on 2020/01/20.
+//  Copyright © 2020 Yohta Watanave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
