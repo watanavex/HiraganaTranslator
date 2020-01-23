a6702213bedab6888b7839dc059618cf45710cda
watanave <susan.se3p@gmail.com>
2020-01-23 14:17:47 +0900

    クリップボードから文字列を取得する処理を実装

--- HiraganaTranslator/view/menu/MenuViewModel.swift
+++ HiraganaTranslator/view/menu/MenuViewModel.swift
@@ -7,6 +7,7 @@
 //
 import UIKit
 import RxSwift
+import MobileCoreServices
 
 class MenuViewModel: AutoGenerateViewModel {
 
@@ -30,4 +31,19 @@ class MenuViewModel: AutoGenerateViewModel {
     }
 
     // MARK: - Processor
+    func getStringFromPasteboard() {
+        let errorMessage = "ひらがなに へんかんしたい ぶんしょうを こぴーしてね"
+        self.dispatcher.onNext(.pasteboardResult(.uninitialized))
+        
+        let value = UIPasteboard.general.value(forPasteboardType: kUTTypeUTF8PlainText as String)
+        guard let planeText = value as? String else {
+            self.dispatcher.onNext(.pasteboardResult(.fail(errorMessage: errorMessage)))
+            return
+        }
+        if planeText.isEmpty {
+            self.dispatcher.onNext(.pasteboardResult(.fail(errorMessage: errorMessage)))
+            return
+        }
+        self.dispatcher.onNext(.pasteboardResult(.some(pastBoardString: planeText)))
+    }
 }
